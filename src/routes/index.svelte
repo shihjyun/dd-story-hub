0fe2fedf812898ab54d1823c08de76b5d351ec65
Steven Yeo <shihjyun.yeo@gmail.com>
2021-06-20 22:34:16 +0800

    fix typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export async function load({ fetch }) {
-    const res = await fetch('http://localhost:3000/article.json?limit=3')
+    const res = await fetch('/article.json?limit=3')
     const data = await res.json()
     return { props: { data } }
   }
