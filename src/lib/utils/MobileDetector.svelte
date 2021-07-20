551f758706641c7b33c86049d768d60906fa85dd
Steven Yeo <shihjyun.yeo@gmail.com>
2021-07-20 22:47:20 +0800

    add MobileDetector

---
+++ src/lib/utils/MobileDetector.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { isMobile } from './MobileDetector.js'
+
+  export let minWidth = 768
+  // media query event handler
+  if (matchMedia) {
+    const mq = window.matchMedia(`(min-width: ${minWidth}px)`)
+    mq.addListener(WidthSizeChange)
+    WidthSizeChange(mq)
+  }
+
+  // mobile detector when window's width size change
+  function WidthSizeChange(mq) {
+    if (mq.matches) {
+      isMobile.set(false)
+    } else {
+      isMobile.set(true)
+    }
+  }
+</script>
