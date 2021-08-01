57bf96253c579db84bfb434d6afd51bdc1ad6877
Steven Yeo <shihjyun.yeo@gmail.com>
2021-08-01 15:24:34 +0800

    feat: add homepage metadata

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,10 +18,30 @@
 </style>
 
 <svelte:head>
-  <title>DD Story Hub</title>
-  <!-- <meta property="og:url" content="https://www.dd-story-hub.com" />
-  <link rel="canonical" href="https://www.dd-story-hub.com" /> -->
-  <!-- <meta name="description" content="" /> -->
+  <title>DD Story Hub 融數基地</title>
+  <link rel="canonical" href="https://www.ddstoryhub.com/" />
+  <meta
+    name="description"
+    content="DD Story Hub 融數基地是因為喜歡研究數位敘事及資料新聞，而聚集在一起的夥伴，期待能夠透過不同的敘事方式，放大故事影響力。"
+  />
+  <meta name="author" content="ddstoryhub.com" />
+  <meta property="og:title" content="DD Story Hub 融數基地" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://www.dd-story-hub.com" />
+  <meta property="og:image" content="https://www.ddstoryhub.com/assets/index/DD-avatar-text-en.png" />
+  <meta property="og:site_name" content="DD Story Hub 融數基地" />
+  <meta
+    property="og:description"
+    content="DD Story Hub 融數基地是因為喜歡研究數位敘事及資料新聞，而聚集在一起的夥伴，期待能夠透過不同的敘事方式，放大故事影響力。"
+  />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="Social" />
+  <meta name="twitter:title" content="DD Story Hub 融數基地" />
+  <meta
+    property="twitter:description"
+    content="DD Story Hub 融數基地是因為喜歡研究數位敘事及資料新聞，而聚集在一起的夥伴，期待能夠透過不同的敘事方式，放大故事影響力。"
+  />
+  <meta name="twitter:image" content="https://www.ddstoryhub.com/assets/index/DD-avatar-text-en.png" />
 </svelte:head>
 
 <section>
