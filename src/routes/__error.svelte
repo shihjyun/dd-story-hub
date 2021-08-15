5a2dd3df5d527439051dce0bbd010a1fe21a79bc
Steven Yeo <shihjyun.yeo@gmail.com>
2021-08-15 18:41:28 +0800

    style: add error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,48 @@
 <style>
-  p {
+  .container {
+    padding: var(--space-4) var(--space-4) var(--space-8) var(--space-4);
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: var(--font-size-7);
+    color: var(--grey-7);
+    margin-bottom: var(--space-5);
+  }
+
+  .container > p {
     text-align: center;
+    font-size: var(--font-size-3);
+    line-height: var(--line-height-body);
+    color: var(--grey-8);
+    padding-bottom: var(--space-4);
+  }
+
+  img {
+    width: 200px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 768px) {
+    .container {
+      padding: 0 0 var(--space-8) 0;
+      width: 85vw;
+    }
+
+    img {
+      width: 300px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .container {
+      width: 75vw;
+      max-width: 1200px;
+    }
   }
 </style>
 
-<p>ヾ(;ﾟ;Д;ﾟ;)ﾉﾞヾ(;ﾟ;Д;ﾟ;)ﾉﾞヾ(;ﾟ;Д;ﾟ;)ﾉﾞ</p>
+<div class="container">
+  <a href="/"><img src="../assets/index/DD-avatar-gray-text-en.svg" alt="dd-logo" /></a>
+  <p>這個頁面目前不存在哦 இдஇ</p>
+</div>
